1b7dd7ffb1cf2e6a7694561727f317d96fc4361c
Cvarog <uchihamilord@gmail.com>
2013-03-21 02:46:14 +0400

    Add admin and templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'mysite.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = ('/home/cvarog/workspace/django_project/mysite/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
